3b2fab43bcedfa78c4975adcfe3d0141057b243a
Vlad Larin <vlad@qooco.com.ua>
2017-04-12 10:27:57 +0300

    [f] Fixed cluster configuration in simple example

--- simple/src/main/java/org/royrusso/app/System.java
+++ simple/src/main/java/org/royrusso/app/System.java
@@ -1,23 +1,24 @@
-/**
-  * (C) Copyright 2014 Roy Russo
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  */
+/*
+ * (C) Copyright 2014 Roy Russo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 package org.royrusso.app;
 
 import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import org.royrusso.actor.SimpleActor;
@@ -30,13 +31,24 @@ import org.slf4j.LoggerFactory;
  */
 public class System {
 
-    public static final Logger log = LoggerFactory.getLogger(System.class);
+    private static final Logger log = LoggerFactory.getLogger(System.class);
 
     public static void main(String... args) throws Exception {
 
-        final ActorSystem actorSystem = ActorSystem.create("actor-system");
+        final ActorSystem actorSystem = ActorSystem.create("ClusterSystem");
 
         Thread.sleep(5000);
+
+        if (args.length > 0) {
+
+            final ActorSelection simpleActorRef = actorSystem.actorSelection(
+                    "akka.tcp://ClusterSystem@127.0.0.1:2553/user/simple-actor");
+
+            simpleActorRef.tell(new Command("Hello from daddy!"), null);
+
+            actorSystem.awaitTermination();
+            return;
+        }
 
         final ActorRef actorRef = actorSystem.actorOf(Props.create(SimpleActor.class), "simple-actor");
 
@@ -46,7 +58,7 @@ public class System {
         actorRef.tell(new Command("CMD 4"), null);
         actorRef.tell(new Command("CMD 5"), null);
 
-        Thread.sleep(5000);
+        Thread.sleep(50000);
 
         log.debug("Actor System Shutdown Starting...");
 
